4c292311993db75e12d320c4937da2db0263d478
Elina <elina@MacBook-Pro-Elina.local>
2025-10-03 21:09:35 +0300

    develop

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -4,7 +4,7 @@ import './CommentList.scss';
 export const CommentList = ({ comments }) => (
   <div className="CommentList">
     {comments.map(comment => (
-      <CommentInfo comment={comment} key={comment.postId} />
+      <CommentInfo comment={comment} key={comment.id} />
     ))}
   </div>
 );
